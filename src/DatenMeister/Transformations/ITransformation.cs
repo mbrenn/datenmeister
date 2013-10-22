a3080052b48d9a2cad27e7ae424b5ec01877e8f5
Martin Brenn <brenn@depon.net>
2013-10-22 19:41:13 +0200

    Definition of the interface for transformations

---
+++ src/DatenMeister/Transformations/ITransformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatenMeister.Transformations
+{
+    public interface ITransformation: IURIExtent
+    {
+        IURIExtent source
+        {
+            get;
+            set;
+        }
+    }
+}
