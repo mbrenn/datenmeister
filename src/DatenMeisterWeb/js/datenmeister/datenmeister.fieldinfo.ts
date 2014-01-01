05baafd73b66349a2a75b67d42c2239b2aedfe6c
Martin Brenn <brenn@depon.net>
2014-01-01 23:28:20 +0100

    Start of implementation for fields

--- src/DatenMeisterWeb/js/datenmeister/datenmeister.fieldinfo.ts
+++ src/DatenMeisterWeb/js/datenmeister/datenmeister.fieldinfo.ts
@@ -1,10 +1,153 @@
 
 import d = require("datenmeister.objects");
 
-export module Text
-{
-    export function create() : d.JsonExtentObject
+/* 
+ * Describes the most generic functions
+ */
+export module General
+{    
+    export function setTitle(value: d.JsonExtentObject, title : string)
     {
-        return true;
+        value.set("title", title);
+    }
+    
+    export function setName(value : d.JsonExtentObject, name: string)
+    {
+        value.set("name", name);
+    }
+    
+    export function setReadOnly(value : d.JsonExtentObject, isReadOnly: boolean)
+    {
+        value.set("readonly", isReadOnly);
+    }    
+    
+    export function getTitle(value: d.JsonExtentObject)
+    {
+        return value.get("title");
+    }
+    
+    export function getName(value: d.JsonExtentObject)
+    {
+        return value.get("name");
+    }
+    
+    export function getReadOnly(value: d.JsonExtentObject)
+    {
+        return value.get("readonly");
+    }    
+}
+
+/*
+ * Describes one field element, that is just a read-only description text with pre-default content
+ * This field will contain the title on the left column and the commentText as Encoded Text in table
+ */
+export module Comment
+{
+    export function create(title : string, commentText? : string) : d.JsonExtentObject
+    {
+        var result = new d.JsonExtentObject();
+        
+        if ( title !== undefined )
+        {
+            setTitle(result, title);
+        }
+        
+        if ( commentText !== undefined )
+        {
+            setComment(result, commentText);
+        }
+        
+        result.set("type", "DatenMeister.DataFields.Comment");
+        
+        return result;
+    }
+    
+    export function setTitle(value: d.JsonExtentObject, title : string)
+    {
+        General.setTitle(value, title);
+    }
+    
+    export function setComment(value: d.JsonExtentObject, commentText : string)
+    {
+        value.set("comment", commentText);
+    }
+}
+
+/*
+ * Describes one textfield element, which has an input field and can be shown to the user.
+ * It also contains information about height and width of the field.
+ * - title: Title being shown to user
+ * - name: Name of the property, where value will be stored
+ * - readonly: True, if field is read-only
+ */
+export module TextField
+{
+    export function create(title : string, key : string) : d.JsonExtentObject
+    {
+        var result = new d.JsonExtentObject();
+        
+        if ( title !== undefined )
+        {
+            setTitle(result, title);
+        }
+        
+        if ( key !== undefined )
+        {
+            setName(result, key);
+        }
+        
+        result.set("type", "DatenMeister.DataFields.TextField");
+        
+        return result;
+    }
+    
+    export function setTitle(value : d.JsonExtentObject, title : string)
+    {
+        General.setTitle(value, title);
+    }
+    
+    export function getTitle(value: d.JsonExtentObject)
+    {
+        General.getTitle(value);
+    }
+    
+    export function setName(value : d.JsonExtentObject, name: string)
+    {
+        General.setName(value, name);
+    }
+    
+    export function getName(value: d.JsonExtentObject)
+    {
+        General.getName(value);
+    }
+    
+    export function setReadOnly(value: d.JsonExtentObject, isReadOnly: boolean)
+    {
+        General.setReadOnly(value, isReadOnly);
+    }
+    
+    export function getReadOnly(value: d.JsonExtentObject)
+    {
+        General.getReadOnly(value);
+    }
+    
+    export function setWidth(value : d.JsonExtentObject, width : number)
+    {
+        value.set("width", width);
+    }
+    
+    export function getWidth(value : d.JsonExtentObject)
+    {
+        return value.get("width");
+    }
+    
+    export function setHeight(value : d.JsonExtentObject, height : number)
+    {
+        value.set("height", height);
+    }
+    
+    export function getHeight(value : d.JsonExtentObject)
+    {
+        return value.get("height");
     }
 }
