ee4e1251889751aed974c19313986bc8a37ff78d
Martin Brenn <brenn@depon.net>
2014-05-13 21:13:17 -0400

    Interfaces for IReflectiveCollection and IReflectiveSequence.cs

---
+++ src/DatenMeister/IReflectiveCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DatenMeister
+{
+    public interface IReflectiveCollection : IEnumerator<object>
+    {
+        bool add(object value);
+
+        bool addAll(object value);
+
+        void clear();
+
+        object remove(object value);
+
+        int size();
+    }
+}
---
+++ src/DatenMeister/IReflectiveSequence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DatenMeister
+{
+    public interface IReflectiveSequence : IReflectiveCollection
+    {
+        void add(int index, object value);
+
+        object get(int index);
+
+        object remove(int index);
+
+        object set(int index, object value);
+    }
+}
