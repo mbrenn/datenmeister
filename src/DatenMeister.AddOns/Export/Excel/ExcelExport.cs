6784418a6fae1d3a54b13bcc0c0766b87166793f
Martin Brenn <brenn@depon.net>
2014-05-24 15:11:05 -0400

    I got my first Excel file!

--- src/DatenMeister.AddOns/Export/Excel/ExcelExport.cs
+++ src/DatenMeister.AddOns/Export/Excel/ExcelExport.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using BurnSystems.Test;
+using NPOI.XSSF.UserModel;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DatenMeister.AddOns.Export.Excel
@@ -10,6 +15,31 @@ namespace DatenMeister.AddOns.Export.Excel
     {
         public void ExportToFile(IURIExtent extent, ExcelExportSettings settings)
         {
+            // Pop old culture and set to US, due to problems in NPOI
+            var oldCulture = Thread.CurrentThread.CurrentCulture;
+            var oldUICulture = Thread.CurrentThread.CurrentUICulture;
+
+            Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+
+            // Create the sheet
+            var workbook = new XSSFWorkbook();
+            var sheet = workbook.CreateSheet("FirstSheet");
+
+            var row = sheet.CreateRow(1);
+            Ensure.That(row != null, "Row is null");
+            var cell = row.CreateCell(5);
+
+            Ensure.That(row != null, "Cell is null");
+            cell.SetCellValue("Hallo, erster Inhalt!");
+
+            using ( var fileStream = new FileStream ( settings.Path, FileMode.Create))
+            {
+                workbook.Write(fileStream);
+            }
+
+            // Restore the culture
+            Thread.CurrentThread.CurrentCulture = oldCulture;
+            Thread.CurrentThread.CurrentUICulture = oldUICulture;
         }
     }
 }
