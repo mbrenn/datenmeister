12e8a2cdc6d05c4d1958c9c9000412077eedd053
Martin Brenn <brenn@depon.net>
2014-05-26 16:32:16 -0400

    Creation of GenericFactory

---
+++ src/DatenMeister/DataProvider/GenericFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatenMeister.DataProvider
+{
+    /// <summary>
+    /// Defines the factory 
+    /// </summary>
+    public class GenericFactory : IFactory
+    {
+        public IURIExtent Extent
+        {
+            get;
+            set;
+        }
+
+        public GenericFactory(IURIExtent extent)
+        {
+            this.Extent = extent;
+        }
+
+        /// <summary>
+        /// Creates the object
+        /// </summary>
+        /// <param name="type">Type of the object to be reated</param>
+        /// <returns>Object being created</returns>
+        public IObject create(IObject type)
+        {
+            var obj = new GenericElement(this.Extent, type: type);
+            return obj;
+        }
+
+        public IObject createFromString(IObject dataType, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string convertToString(IObject dataType, IObject value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
