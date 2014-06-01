42c96fe2165c996731cfb12d385cee095c1bb5c9
Martin Brenn <brenn@depon.net>
2014-06-01 13:42:05 -0400

    Creates thw window

---
+++ src/DatenMeister.WPF/Windows/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using DatenMeister.Logic.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatenMeister.WPF.Windows
+{
+    /// <summary>
+    /// Contains several extension methods for windows
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Creates the window for the DatenMeister. 
+        /// </summary>
+        /// <param name="core">Application of the window</param>
+        /// <returns>Returned window</returns>
+        public static IDatenMeisterWindow CreateWindow(this ApplicationCore core)
+        {
+            var wnd = new DatenMeisterWindow(core);
+
+            // Just sets the title and shows the Window
+            wnd.Show();
+            wnd.RefreshTab();
+
+            return wnd;
+        }
+    }
+}
