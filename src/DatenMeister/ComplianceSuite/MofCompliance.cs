54c74edbb142a5991b9d634f364c9d95b3f9765a
Martin Brenn <brenn@depon.net>
2014-08-17 18:59:21 -0400

    Update

---
+++ src/DatenMeister/ComplianceSuite/MofCompliance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatenMeister.ComplianceSuite
+{
+    public class MofCompliance
+    {
+        /// <summary>
+        /// Stores the result of the storage
+        /// </summary>
+        private IObject resultStorage;
+
+        /// <summary>
+        /// Empty test object, where getting and setting will be 
+        /// executed and tested.
+        /// </summary>
+        private IObject testObject;
+
+        public MofCompliance(IObject testObject, IObject resultStorage)
+        {
+            this.testObject = testObject;
+            this.resultStorage = resultStorage;
+        }
+
+        public void ExecuteTests()
+        {
+        }
+    }
+}
