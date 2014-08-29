82bfaa9a92fda685cc2b3dad7f707e16888db165
Martin Brenn <brenn@depon.net>
2014-08-28 22:38:59 -0400

    Wrapper is here...

--- src/DatenMeister/DataProvider/Wrapper/WrapperExtent.cs
+++ src/DatenMeister/DataProvider/Wrapper/WrapperExtent.cs
@@ -6,11 +6,162 @@ using System.Threading.Tasks;
 
 namespace DatenMeister.DataProvider.Wrapper
 {
+    public interface IWrapperExtent : IURIExtent
+    {
+        object Convert(object value);
+    }
+
     /// <summary>
     /// 
     /// </summary>
-    /// <typeparam name="TExtent"></typeparam>
-    class WrapperExtent<TExtent> : IURIExtent where TExtent : IURIExtent
+    /// <typeparam name="TReflectiveSequence">Type of the wrapped reflective sequence</typeparam>
+    /// <typeparam name="TElement">Type of the </typeparam>
+    public class WrapperExtent<TReflectiveSequence, TElement, TUnspecified> : IWrapperExtent 
+        where TReflectiveSequence : WrapperReflectiveSequence, new()
+        where TElement : WrapperElement, new()
+        where TUnspecified : WrapperUnspecified, new ()
     {
+        /// <summary>
+        /// Stores the inner extent
+        /// </summary>
+        private IURIExtent inner;
+
+        protected IURIExtent Inner
+        {
+            get { return this.inner; }
+            set { this.inner = value; }
+        }
+
+        protected WrapperExtent()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the WrapperExtent class
+        /// </summary>
+        /// <param name="extent">Extent to be wrapped</param>
+        public WrapperExtent(IURIExtent extent)
+        {
+            this.inner = extent;
+        }
+
+        /// <summary>
+        /// Gets the wrapped original extent
+        /// </summary>
+        /// <returns></returns>
+        public IURIExtent Unwrap()
+        {
+            return this.inner;
+        }
+
+        /// <summary>
+        /// Wraps the extent
+        /// </summary>
+        /// <param name="extent">Extent to be wrapped</param>
+        /// <returns>The wrapped instance</returns>
+        public static WrapperExtent<TReflectiveSequence, TElement, TUnspecified> Wrap(IURIExtent extent)
+        {
+            return new WrapperExtent<TReflectiveSequence, TElement, TUnspecified>(extent);
+        }
+
+        public virtual string ContextURI()
+        {
+            return this.inner.ContextURI();
+        }
+
+        public virtual IReflectiveSequence Elements()
+        {
+            return this.CreateReflectiveSequence(this.inner.Elements());
+        }
+
+        public virtual IPool Pool
+        {
+            get
+            {
+                return this.inner.Pool;
+            }
+            set
+            {
+                this.inner.Pool = value;
+            }
+        }
+
+        public virtual bool IsDirty
+        {
+            get
+            {
+                return this.inner.IsDirty;
+            }
+            set
+            {
+                this.inner.IsDirty = value;
+            }
+        }
+
+        /// <summary>
+        /// Creates an instance of the reflective instance
+        /// </summary>
+        /// <returns>Created reflective sequence</returns>
+        public IReflectiveSequence CreateReflectiveSequence(IReflectiveSequence sequence)
+        {
+            var result = new TReflectiveSequence();
+            result.WrapperExtent = this;
+            result.Inner = sequence;
+            return result;
+        }
+
+        /// <summary>
+        /// Creates an instance of the reflective instance
+        /// </summary>
+        /// <returns>Created reflective sequence</returns>
+        public IElement CreateElement(IElement element)
+        {
+            var result = new TElement();
+            result.WrapperExtent = this;
+            result.Inner = element;
+            return result;
+        }
+
+        /// <summary>
+        /// Creates an instance of the reflective instance
+        /// </summary>
+        /// <returns>Created reflective sequence</returns>
+        public IUnspecified CreateUnspecified(IUnspecified element)
+        {
+            var result = new TUnspecified();
+            result.WrapperExtent = this;
+            result.Inner = element;
+            return result;
+        }
+
+        /// <summary>
+        /// Converts the object to wrapped instances if necessary. 
+        /// </summary>
+        /// <param name="value">Value to be converted</param>
+        /// <returns>Converted error</returns>
+        public object Convert(object value)
+        {
+            if (Extensions.IsNative(value))
+            {
+                return value;
+            }
+
+            if (value is IElement)
+            {
+                return this.CreateElement(value as IElement);
+            }
+
+            if (value is IReflectiveSequence)
+            {
+                return this.CreateReflectiveSequence(value as IReflectiveSequence);
+            }
+
+            if (value is IUnspecified)
+            {
+                return this.CreateUnspecified(value as IUnspecified);
+            }
+
+            throw new NotImplementedException("Cannot conver type: " + value.ToString());
+        }
     }
 }
---
+++ src/DatenMeister/DataProvider/Wrapper/WrapperUnspecified.cs
@@ -0,0 +1,85 @@
+﻿using BurnSystems.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatenMeister.DataProvider.Wrapper
+{
+    /// <summary>
+    /// Wraps the IUnspecified class
+    /// </summary>
+    public class WrapperUnspecified : IUnspecified
+    {
+        /// <summary>
+        /// Stores the inner element
+        /// </summary>
+        private IUnspecified inner;
+
+        internal IUnspecified Inner
+        {
+            get { return this.inner; }
+            set { this.inner = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the wrapped extent
+        /// </summary>
+        internal IWrapperExtent WrapperExtent
+        {
+            get;
+            set;
+        }
+
+        public WrapperUnspecified()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the WrapperElement class
+        /// </summary>
+        /// <param name="inner">Element to be added</param>
+        public WrapperUnspecified(IWrapperExtent extent, IUnspecified inner)
+        {
+            this.WrapperExtent = extent;
+            this.inner = inner;
+        }
+
+        public IUnspecified Unwrap()
+        {
+            return this.inner;
+        }
+
+        public PropertyValueType PropertyValueType
+        {
+            get
+            {
+                return this.Inner.PropertyValueType;
+            }
+            set
+            {
+                this.Inner.PropertyValueType = value;
+            }
+        }
+
+        public object AsSingle()
+        {
+            return this.WrapperExtent.Convert(this.Inner.AsSingle());
+        }
+
+        public IReflectiveCollection AsReflectiveCollection()
+        {
+            var result = this.WrapperExtent.Convert(this.Inner.AsReflectiveCollection()) as IReflectiveCollection;
+            Ensure.That(result != null, "Wrapper Extent did not return a reflective collection");
+            return result;
+        }
+
+        public IReflectiveSequence AsReflectiveSequence()
+        {
+            var result = this.WrapperExtent.Convert(this.Inner.AsReflectiveSequence()) as IReflectiveSequence;
+            Ensure.That(result != null, "Wrapper Extent did not return a reflective sequence");
+            return result;
+        }
+    }
+}
