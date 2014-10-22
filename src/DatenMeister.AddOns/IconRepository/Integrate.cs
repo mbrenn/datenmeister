1869627909fbfa57c068743cb9594833ec3de73e
Martin Brenn <brenn@depon.net>
2014-10-21 23:36:55 -0400

    External icons are working. Need to be completed

--- src/DatenMeister.AddOns/IconRepository/Integrate.cs
+++ src/DatenMeister.AddOns/IconRepository/Integrate.cs
@@ -1,7 +1,10 @@
-﻿using DatenMeister.Logic;
+﻿using BurnSystems.Test;
+using DatenMeister.Logic;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,10 +20,32 @@ namespace DatenMeister.AddOns.IconRepository
         /// </summary>
         public static void Perform(ApplicationCore core)
         {
-            core.ViewSetInitialized += (x, y) =>
+            if (File.Exists("DatenMeister.Icons.dll"))
+            {
+                var dllPath = Path.Combine(Environment.CurrentDirectory, "DatenMeister.Icons.dll");
+                var assembly = Assembly.LoadFile(dllPath);
+                Ensure.That(assembly != null, "'DatenMeister.Icons.dll' could not be loaded");
+
+                var type = assembly.GetType("DatenMeister.Icons.NiceIconsRepository");
+                Ensure.That(type != null, "Type 'DatenMeister.Icons.NiceIconsRepository' is not found");
+
+                var method = type.GetMethod("Free");
+                Ensure.That(method != null, "Method Free was expected");
+                method.Invoke(null, new object[] { "Accept Axialis Icons" });
+
+                core.ViewSetInitialized += (x, y) =>
                 {
-                    Injection.Application.Bind<IIconRepository>().To<GplIconRepository>();
+                    Injection.Application.Bind<IIconRepository>().To(type);
                 };
+            }
+            else
+            {
+                core.ViewSetInitialized += (x, y) =>
+                    {
+                        Injection.Application.Bind<IIconRepository>().To<GplIconRepository>();
+                    };
+            }
         }
     }
 }
+
