a8acb82c88d64ee558ca7d333df8a588ab4f8b0f
Martin Brenn <brenn@depon.net>
2014-11-16 10:40:24 -0500

    Introduced a single method where an application can be started, will still be extended

---
+++ src/DatenMeister.AddOns/DefaultModules.cs
@@ -0,0 +1,79 @@
+﻿using DatenMeister.AddOns.Export.Excel;
+using DatenMeister.AddOns.Export.Report.Simple;
+using DatenMeister.AddOns.Views;
+using DatenMeister.Logic;
+using DatenMeister.WPF.Modules.RecentFiles;
+using DatenMeister.WPF.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DatenMeister.AddOns
+{
+    public static class DefaultModules
+    {
+        /// <summary>
+        /// Performs a default start of the DatenMeister with all plugins
+        /// </summary>
+        public static StartResult DefaultStartWith<T>(Application application) where T : IDatenMeisterSettings, new()
+        {
+            var core = new ApplicationCore();
+            DatenMeister.WPF.Modules.IconRepository.Integrate.Perform(core);
+            core.Start<T>();
+            var wnd = WindowFactory.CreateWindow(core);
+
+            // Other menu helpers
+            RecentFileIntegration.AddSupport(wnd);
+            AllExtentView.AddExtentView(wnd);
+
+            // Exports the entry to an excel item
+            ExcelExportGui.AddMenu(wnd);
+            TypeManager.Integrate(wnd);
+            ViewSetManager.Integrate(wnd);
+            SimpleReportGui.Integrate(wnd);
+            DatenMeister.AddOns.ComplianceSuite.WPF.Plugin.Integrate(wnd);
+
+            // Registers to exit event of the application
+            if (application != null)
+            {
+                if (core.Settings != null)
+                {
+                    core.StoreViewSet();
+                }
+            }
+
+            return new StartResult()
+            {
+                Core = core,
+                Window = wnd
+            };
+        }
+
+        /// <summary>
+        /// Defines a class, containing the result of start of default modules
+        /// </summary>
+        public class StartResult
+        {
+            /// <summary>
+            /// Gets or sets the created application core
+            /// </summary>
+            public ApplicationCore Core
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// Gets or sets the datenmeister window
+            /// </summary>
+            public IDatenMeisterWindow Window
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
