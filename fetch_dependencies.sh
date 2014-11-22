0cb809448a2b465c9c93393109456e1918a97cec
Martin Brenn <brenn@depon.net>
2014-11-22 10:28:41 -0500

    Add fetch dependencies for Linux

---
+++ fetch_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo Loading the complete dependencies
+
+cd ..
+git clone https://github.com/mbrenn/burnsystems.git
+git clone https://github.com/mbrenn/burnsystems.flexbg.git
+git clone https://github.com/mbrenn/burnsystems.parser.git
+git clone https://github.com/mbrenn/burnsystems.webserver
+git clone https://github.com/mbrenn/burnsystems.webserver.umbra
+git clone https://github.com/mbrenn/projektmeister.git
+git clone https://github.com/mbrenn/dejs
+
+
+
