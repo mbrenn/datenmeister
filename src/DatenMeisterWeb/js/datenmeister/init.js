71be031cfa6e2d189bbb513ca30b36f6d296e851
Martin Brenn <brenn@depon.net>
2015-05-09 12:55:10 -0400

    Remove the current Web Project

--- src/DatenMeisterWeb/js/datenmeister/init.js
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="../requirejs/require.d.ts" />
-/// <reference path="datenmeister.ts" />
-define(["require", "exports", "datenmeister"], function(require, exports, dm) {
-    $(document).ready(function () {
-        // initializes the dom
-        dm.init();
-    });
-});
-//# sourceMappingURL=init.js.map
--- src/DatenMeisterWeb/js/dejs/dejs.number.js
+++
@@ -1,9 +0,0 @@
-﻿define(["require", "exports"], function(require, exports) {
-    var Number = (function () {
-        function Number() {
-        }
-        return Number;
-    })();
-    exports.Number = Number;
-});
-//# sourceMappingURL=dejs.number.js.map
